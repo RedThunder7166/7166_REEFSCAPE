2287c6f2cf2b287f491f60efe0884ed88fc5a362
RedThunder7166 <LHSRobotics7166@gmail.com>
2025-02-15 16:56:31 -0500

    remove intake outtake config

--- src/main/java/frc/robot/subsystems/IntakeOuttakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeOuttakeSubsystem.java
@@ -39,16 +39,6 @@ public class IntakeOuttakeSubsystem extends SubsystemBase {
         // FIXME: tune intake outtake PID
         TalonFXConfiguration configs = new TalonFXConfiguration();
 
-        /* Voltage-based velocity requires a velocity feed forward to account for the back-emf of the motor */
-        configs.Slot0.kS = 0.1; // To account for friction, add 0.1 V of static feedforward
-        configs.Slot0.kV = 0.12; // Kraken X60 is a 500 kV motor, 500 rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / rotation per second
-        configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 0.11 V output
-        configs.Slot0.kI = 0; // No output for integrated error
-        configs.Slot0.kD = 0; // No output for error derivative
-        // Peak output of 8 volts
-        configs.Voltage.withPeakForwardVoltage(Volts.of(8))
-            .withPeakReverseVoltage(Volts.of(-8));
-
         OurUtils.tryApplyConfig(m_scoreMotor, configs);
 
         // TODO: intakeMotor should be controlled entirely through sensors, so remove this
